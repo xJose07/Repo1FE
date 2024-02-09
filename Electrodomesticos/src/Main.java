13e8c92f42fce8b665e6a4c7394af54004250dae
Pau Guillen <paugctrabajo@gmail.com>
2024-02-09 13:55:25 +0100

    Added project Electrodomesticos

---
+++ Electrodomesticos/src/Main.java
@@ -0,0 +1,12 @@
+import com.model.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Electrodomestico[] lista=new Electrodomestico[10];
+        lista=MisFunciones.llenarLista(lista);
+        Electrodomestico e = MisFunciones.crearElectrodomestico();
+        System.out.println(e.toString());
+        //System.out.println(MisFunciones.mostrarPreciosFinalesDeCadaElectrodomestico(lista));
+        //System.out.println(MisFunciones.mostrarPrecioFinal(lista));
+    }
+}
